62f120d2180dbfbf2745b757a3620d93835d6ce1
Johora-Juhi <zohrajuhi1012@gmail.com>
2022-11-24 01:37:48 +0600

    mongodb set up

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 
-// middleware 
+// middlewre 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bkdzfxe.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+    try{
+
+    }
+    finally{
+
+    }
+}
+
+
 
 app.get('/',(req,res)=>{
     res.send('We sell server is Running here');
